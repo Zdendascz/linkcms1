d27a0f779b11bc02ec85c80e824a9a1285af8b9b
Zdeněk Klusák <zdenek.klusak@gmail.com>
2024-01-09 19:03:23 +0100

    Podařilo se rozchodit propojení lincmslib a databáze eloquent na linkcms

---
+++ models/User.php
@@ -0,0 +1,12 @@
+<?php
+namespace linkcms1\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class User extends Model
+{
+    protected $table = 'users';
+    // Ostatní definice modelu...
+}
+
+?>
