5562181512eb15294b13af34ae273d56b90477ba
Zdeněk Klusák <zdenek.klusak@gmail.com>
2024-01-17 00:32:00 +0100

    Okomentovány funkce a doplněny debug a tracy report

--- class/control.php
+++ class/control.php
@@ -6,9 +6,13 @@ use linkcms1\Models\Site;
 use linkcms1\Models\Url;
 use linkcms1\Models\Category;
 use Monolog\Logger;
+use Tracy\Debugger;
 
-
-
+Debugger::enable(Debugger::DEVELOPMENT);
+/**
+ * Třída reprezentující web, zpracovává kontrolu domény a informace o ní.
+ * také zpracovává informace o konkrétní stránce
+ */
 class domainControl {
 
     protected $capsule;
@@ -17,11 +21,18 @@ class domainControl {
     public function __construct($capsule, Logger $logger) {
         $this->capsule = $capsule;
         $this->logger = $logger;
+        
 
     }
-
+    
+    /**
+     * loadDomain
+     * funkce načítá infomace o doméně a ukládá je k dalšímu použití
+     * @return data v poli $_SERVER[] s prefixem SITE_
+     */
     public function loadDomain (){
         $domain = Site::where('domain', '=', str_replace("www.","",$_SERVER['HTTP_HOST']))->first();
+        $domainInfo = "";
         if ($domain) {
             foreach ($domain->getAttributes() as $key => $value) {
                 // Kontrola, zda hodnota je JSON a dekódování
@@ -31,14 +42,21 @@ class domainControl {
         
                 // Přidání hodnoty do $_SERVER s prefixem 'SITE_'
                 $_SERVER['SITE_' . strtoupper($key)] = $value;
-            } 
+                $domainInfo .= $key."=".$value;
+            }
+            $this->logger->debug('Načtení informací o doméně '.$_SERVER['HTTP_HOST']." >>> ".$domainInfo); 
         }
         else{
             $this->logger->error('Nepodařilo se načíst data domény '.$_SERVER['HTTP_HOST']);
         }
 
     }
-
+    
+    /**
+     * loadSite
+     * Funkce načítá informace o aktuální URL pro potřeby routování
+     * @return array Kompletní informace o aktuální url adrese
+     */
     public function loadSite() {
         // Rozdělení URL na komponenty a získání pouze cesty
         $parsedUrl = parse_url($_SERVER['REQUEST_URI']);
@@ -51,24 +69,18 @@ class domainControl {
         // Vyhledání URL v databázi, která odpovídá jak doméně, tak cestě
         $url = Url::where('domain', '=', $_SERVER['SITE_DOMAIN'])
                 ->where('url', '=', $path)
-                ->first();
-                
-                 
+                ->first();                
+        $this->logger->debug('Načtení informací o url ze SQL [domain:'.$_SERVER['SITE_DOMAIN']." path:".$path."]");         
     
         // Kontrola, zda byl záznam nalezen
         if ($url === null) {
             // Zde můžete zpracovat situaci, kdy záznam nebyl nalezen
-            echo "Záznam nenalezen: ".$_SERVER['SITE_DOMAIN'].$path;
-            //this->logger->warning('Adresa '.$_SERVER['SITE_DOMAIN'].$path." nenalezena");
+            \Tracy\Debugger::barDump($_SERVER['SITE_DOMAIN'].$path, 'Problém s nalezením url v db');
+            $this->logger->warning('Adresa '.$_SERVER['SITE_DOMAIN'].$path." nenalezena");
 
             // Můžete vrátit null nebo jinou výchozí hodnotu
             return null;
         }
-
-        //echo $_SERVER['REQUEST_URI'];
-        // Zakomentovaný kód pro debug
-        //echo $_SERVER['SITE_DOMAIN'].'<pre>' . print_r($url, true) . '</pre>'.'<pre>' . print_r($_SERVER, true) . '</pre>';
-
         return $url->toArray();
     }
 
